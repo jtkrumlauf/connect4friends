71bdaf782cadbe3496c205c205fdc99e82eb0c5c
hpfoster9 <henrythefoster@gmail.com>
2018-03-11 17:20:47 -0400

    php test

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php if ($_SERVER["REQUEST_METHOD"] == "GET"): ?>
+	<p>In the get</p>
+	 <form action="index.php" method="post">
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Go to post</button>
+        </div>
+	</form>
+<?php endif ?>
+<?php if($_SERVER["REQUEST_METHOD"] == "POST"): ?>
+	<p>In the post<p>
+		<form action="index.php" method="get">
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">shit go back</button>
+        </div>
+	</form>
+<?php endif ?>
+
+</body>
+</html>
